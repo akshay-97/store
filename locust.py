dd7c7f091df1d56a8a643747294e9448f3d56cc2
Akshay S <akshay.s@Akshay-Subramanian-D66TQ6D97K.local>
2024-08-09 17:44:38 +0530

    added grafana locust files

---
+++ locust.py
@@ -0,0 +1,52 @@
+from locust import HttpUser, TaskSet, SequentialTaskSet, task,  between
+import uuid
+import os
+
+config_set = {
+   'version' : 1,
+   'base' : uuid.uuid4().hex
+}
+
+
+class PaymentsBehaviour(SequentialTaskSet):
+    payment_id = None
+    attempt_version = [None] * 10
+    
+    def gen(self):
+        config_set['version'] += 1
+        return (config_set['base'] + str(config_set['version']))
+    @task(1)
+    def intent_create(self):
+        payment_id = self.gen()
+        self.payment_id = payment_id
+        response = self.client.get('/create/' + self.payment_id, name = "IntentCreate")
+    @task(1)
+    def pay(self):
+        for i in range(10):
+            self.attempt_version[i] = self.payment_id + 'version' + str(i)
+            response = self.client.get('/pay/' + self.payment_id + '/' + self.attempt_version[i], name = "AttemptCreate")
+    @task(1)
+    def update_attempt(self):
+        for i in range(len(self.attempt_version)):
+            if self.attempt_version[i] is not None:
+                response = self.client.get('/update_attempt/pay/' + self.attempt_version[i] + '/' + self.payment_id , name = "UpdateAttempt")
+    
+    @task(1)
+    def update_intent(self):
+        response = self.client.get('/update_intent/' + self.payment_id, name = "UpdateIntent")
+    
+    @task(1)
+    def retrieve_attempt(self):
+        response = self.client.get('/retrieve/payment_attempt/' + self.payment_id, name = "RetrieveAllAttempt")
+    
+    @task(1)
+    def retrieve_intent(self):
+        response = self.client.get('/retrieve/payment_intent/' + self.payment_id, name = "RetrieveIntent")
+
+def make():
+    return type("MyClass", (PaymentsBehaviour,), {})
+    
+
+class WebsiteUser(HttpUser):
+    MyClass = make()
+    tasks = [MyClass]
