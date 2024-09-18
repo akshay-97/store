0a5bbfd77533d451053ee0e995a6ee52fd238c1f
kaeshk <adiosphobian@gmail.com>
2024-09-18 18:32:47 +0530

    updated locust to extract cell from response id

--- locust.py
+++ locust.py
@@ -3,6 +3,7 @@ import uuid
 import os
 import signal
 import sys
+import json
 
 config_set = {
    'version' : 1,
@@ -11,9 +12,13 @@ config_set = {
     'pa' : [],
  }
 
+def extract_cell(input):
+    return input.split("_")[0]
+
 class PaymentsBehaviour(SequentialTaskSet):
     payment_id = None
     attempt_version = [None] * 1
+    cell = None
     
     def gen(self):
         config_set['version'] += 1
@@ -24,31 +29,33 @@ class PaymentsBehaviour(SequentialTaskSet):
         self.payment_id = payment_id
         response = self.client.get('/create/' + self.payment_id + '/kaps', name = "IntentCreate",allow_redirects=True)
         if response.status_code == 200:
+            self.cell = extract_cell(json.loads(response.text)["pi"])
             config_set['pi'].append(payment_id)
     @task(1)
     def pay(self):
         for i in range(1):
             self.attempt_version[i] = self.payment_id + 'version' + str(i)
-            response = self.client.get('/pay/' + self.payment_id + '/' + self.attempt_version[i], name = "AttemptCreate",allow_redirects=True)
+            response = self.client.get('/pay/' + self.payment_id + '/' + self.attempt_version[i], name = "AttemptCreate",allow_redirects=True, headers = {'x-region' : self.cell})
             if response.status_code == 200:
+                self.cell = extract_cell(json.loads(response.text)["pa"])
                 config_set['pa'].append(self.payment_id + ',' + self.attempt_version[i] + '\n')
     @task(1)
     def update_attempt(self):
         for i in range(len(self.attempt_version)):
             if self.attempt_version[i] is not None:
-                response = self.client.get('/update_attempt/pay/' + self.attempt_version[i] + '/' + self.payment_id , name = "UpdateAttempt",allow_redirects=True)
+                response = self.client.get('/update_attempt/pay/' + self.attempt_version[i] + '/' + self.payment_id , name = "UpdateAttempt",allow_redirects=True, headers = {'x-region' : self.cell})
     
     @task(1)
     def update_intent(self):
-        response = self.client.get('/update_intent/' + self.payment_id, name = "UpdateIntent",allow_redirects=True)
+        response = self.client.get('/update_intent/' + self.payment_id, name = "UpdateIntent",allow_redirects=True, headers = {'x-region' : self.cell})
     
     # @task(1)
     # def retrieve_attempt(self):
-    #     response = self.client.get('/retrieve/payment_attempt/' + self.payment_id, name = "RetrieveAllAttempt")
+    #     response = self.client.get('/retrieve/payment_attempt/' + self.payment_id, name = "RetrieveAllAttempt", headers = {'x-region' : self.cell})
     
     @task(1)
     def retrieve_intent(self):
-        response = self.client.get('/retrieve/payment_intent/' + self.payment_id, name = "RetrieveIntent",allow_redirects=True)
+        response = self.client.get('/retrieve/payment_intent/' + self.payment_id, name = "RetrieveIntent",allow_redirects=True, headers = {'x-region' : self.cell})
 
 def make():
     return type("MyClass", (PaymentsBehaviour,), {})
